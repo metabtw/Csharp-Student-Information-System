6eff43a0536f6e190eacbfec2d8f18fe56c6593b
btwmeta <137120250+metabtw@users.noreply.github.com>
2024-12-19 16:19:03 +0300

    Student Information System

---
+++ databaseprc/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace databaseprc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtsurname = new System.Windows.Forms.TextBox();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.addbtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.upgradebtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.bdate = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Print = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(170, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(144, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(92, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone Number";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(256, 79);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(163, 20);
+            this.txtname.TabIndex = 3;
+            // 
+            // txtsurname
+            // 
+            this.txtsurname.Location = new System.Drawing.Point(256, 112);
+            this.txtsurname.Name = "txtsurname";
+            this.txtsurname.Size = new System.Drawing.Size(163, 20);
+            this.txtsurname.TabIndex = 4;
+            // 
+            // txtno
+            // 
+            this.txtno.Location = new System.Drawing.Point(256, 48);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(163, 20);
+            this.txtno.TabIndex = 5;
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(470, 46);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(99, 31);
+            this.addbtn.TabIndex = 6;
+            this.addbtn.Text = "Add";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(470, 83);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(99, 31);
+            this.deletebtn.TabIndex = 7;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // upgradebtn
+            // 
+            this.upgradebtn.Location = new System.Drawing.Point(470, 120);
+            this.upgradebtn.Name = "upgradebtn";
+            this.upgradebtn.Size = new System.Drawing.Size(99, 31);
+            this.upgradebtn.TabIndex = 8;
+            this.upgradebtn.Text = "Upgrade";
+            this.upgradebtn.UseVisualStyleBackColor = true;
+            this.upgradebtn.Click += new System.EventHandler(this.upgradebtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 222);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 200);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(127, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Student no";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(133, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Birth Date";
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(256, 147);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(163, 20);
+            this.txtphone.TabIndex = 12;
+            // 
+            // bdate
+            // 
+            this.bdate.Location = new System.Drawing.Point(256, 182);
+            this.bdate.Name = "bdate";
+            this.bdate.Size = new System.Drawing.Size(163, 20);
+            this.bdate.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(470, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Print
+            // 
+            this.Print.Location = new System.Drawing.Point(591, 103);
+            this.Print.Name = "Print";
+            this.Print.Size = new System.Drawing.Size(93, 44);
+            this.Print.TabIndex = 15;
+            this.Print.Text = "Print";
+            this.Print.UseVisualStyleBackColor = true;
+            this.Print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 434);
+            this.Controls.Add(this.Print);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bdate);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.upgradebtn);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.txtno);
+            this.Controls.Add(this.txtsurname);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtsurname;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.Button upgradebtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.DateTimePicker bdate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Print;
+    }
+}
+
---
+++ databaseprc/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Xml.Linq;
+
+
+namespace databaseprc
+{
+    public partial class Form1 : Form
+    {
+        // SQL bağlantı nesnesi
+        SqlConnection baglanti;
+        // SQL komut nesnesi
+        SqlCommand komut;
+        // SQL veri adaptörü
+        SqlDataAdapter da;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Veritabanından öğrenci bilgilerini getirip tabloya dolduran metot
+        void ogrgetir()
+        {
+            // SQL bağlantısını ayarlar ve açar
+            baglanti = new SqlConnection("server=localhost\\SQLEXPRESS;Initial Catalog=student;Integrated Security=SSPI");
+            baglanti.Open();
+
+            // SQL sorgusu ile tüm verileri çeker
+            da = new SqlDataAdapter("SELECT * FROM dbo.ogrtbl", baglanti);
+
+            // Verileri saklamak için DataTable oluşturur
+            DataTable tablo = new DataTable();
+            // DataTable'ı doldurur
+            da.Fill(tablo);
+            // DataGridView’e veriyi atar
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        // Form yüklendiğinde öğrenci verilerini getirir
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ogrgetir(); // Veriyi getirip yükler
+        }
+
+        // DataGridView hücresine tıklayınca öğrenci bilgilerini textboxlara doldurur
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtno.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtname.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtsurname.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtphone.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            bdate.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+        }
+
+        // Yeni öğrenci ekleyen butonun olay metodu
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            // INSERT SQL komutu ile yeni öğrenci ekleme sorgusu oluşturur
+            string sorgu = "INSERT INTO ogrtbl(name,surname,phone,birth_date) VALUES (@name,@surname,@phone,@birth_date)";
+            komut = new SqlCommand(sorgu, baglanti);
+            // Parametrelerle değerleri bağlar
+            komut.Parameters.AddWithValue("@name", txtname.Text);
+            komut.Parameters.AddWithValue("@surname", txtsurname.Text);
+            komut.Parameters.AddWithValue("@phone", txtphone.Text);
+            komut.Parameters.AddWithValue("@birth_date", bdate.Value);
+            // Bağlantıyı açar ve sorguyu çalıştırır
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+            // Yeni öğrenci eklendikten sonra tabloyu günceller
+            ogrgetir();
+
+        }
+
+        // Öğrenciyi silen butonun olay metodu
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM ogrtbl WHERE studentno=@studentno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@studentno", Convert.ToInt32(txtno.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            ogrgetir();
+        }
+
+        // Öğrenci bilgilerini güncelleyen butonun olay metodu
+        private void upgradebtn_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE ogrtbl SET name=@name,surname=@surname,phone=@phone,birth_date=@birth_date WHERE studentno=@studentno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@studentno", Convert.ToInt32(txtno.Text));
+            komut.Parameters.AddWithValue("@name", txtname.Text);
+            komut.Parameters.AddWithValue("@surname", txtsurname.Text);
+            komut.Parameters.AddWithValue("@phone", txtphone.Text);
+            komut.Parameters.AddWithValue("@birth_date", bdate.Value);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            ogrgetir();
+        }
+
+        // Öğrenciyi numarasına göre arayan bul butonunun metodu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string studentNo = txtno.Text; // Aranacak öğrenci numarasını alır
+            baglanti = new SqlConnection("server=localhost\\SQLEXPRESS;Initial Catalog=student;Integrated Security=SSPI");
+
+            try
+            {
+                baglanti.Open(); // SQL bağlantısını açar
+
+                // Öğrenci numarasına göre arama yapmak için SQL sorgusu
+                string sorgu = "SELECT * FROM dbo.ogrtbl WHERE studentno = @studentno";
+                komut = new SqlCommand(sorgu, baglanti);
+                komut.Parameters.AddWithValue("@studentno", studentNo); // Sorguya parametre ekler
+
+                DataTable tablo = new DataTable();
+                da = new SqlDataAdapter(komut);
+                da.Fill(tablo); // DataTable'ı sorgu sonucuyla doldurur
+
+                // Eğer sonuç varsa DataGridView'e ve TextBox'lara doldurur
+                if (tablo.Rows.Count > 0)
+                {
+                    dataGridView1.DataSource = tablo; // Sonucu DataGridView'de gösterir
+
+                    // TextBox'ları ilk kayıt bilgisiyle doldurur
+                    txtno.Text = tablo.Rows[0]["studentno"].ToString();
+                    txtname.Text = tablo.Rows[0]["name"].ToString();
+                    txtsurname.Text = tablo.Rows[0]["surname"].ToString();
+                    txtphone.Text = tablo.Rows[0]["phone"].ToString();
+                    bdate.Text = tablo.Rows[0]["birth_date"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Öğrenci bulunamadı."); // Sonuç yoksa bilgi verir
+                    dataGridView1.DataSource = null; // DataGridView'i temizler
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda kullanıcıya bilgi verir
+                MessageBox.Show("Hata:Bu şekilde kayıtlı bir öğrenci yok " + ex.Message);
+            }
+            finally
+            {
+                baglanti.Close(); // Bağlantıyı kapatır
+            }
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            // PDF dosyasının kaydedileceği yol
+            string filePath = @"C:\Users\muham\OneDrive\Desktop\OgrenciIzinBelgesi.pdf";
+
+            try
+            {
+                // PDF dokümanını oluştur
+                Document pdfDoc = new Document(PageSize.A4, 50, 50, 50, 50);
+                PdfWriter.GetInstance(pdfDoc, new FileStream(filePath, FileMode.Create));
+                pdfDoc.Open();
+
+                // Türkçe karakter desteği için font ayarları
+                string fontPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "arial.ttf");
+                BaseFont bf = BaseFont.CreateFont(fontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                iTextSharp.text.Font titleFont = new iTextSharp.text.Font(bf, 18, iTextSharp.text.Font.BOLD);
+                iTextSharp.text.Font bodyFont = new iTextSharp.text.Font(bf, 12, iTextSharp.text.Font.NORMAL);
+
+                // Başlık
+                Paragraph title = new Paragraph("Öğrenci İzin Belgesi\n\n", titleFont)
+                {
+                    Alignment = Element.ALIGN_CENTER
+                };
+                pdfDoc.Add(title);
+
+                // TextBox'lardan alınan veriler
+                string studentName = txtname?.Text.Trim() ?? "Adı Belirtilmemiş";
+                string studentSurname = txtsurname?.Text.Trim() ?? "Soyadı Belirtilmemiş";
+                string studentNo = txtno?.Text.Trim() ?? "Numarası Belirtilmemiş";
+
+                // İçerik
+                string izinMetni = $"Okulumuz öğrencilerinden {studentName} {studentSurname}, " +
+                                   $"öğrenci numarası {studentNo}, " +
+                                   $"bu gün ({DateTime.Now.ToShortDateString()}) izinlidir.\n\n" +
+                                   $"İlgililere duyurulur.";
+
+                Paragraph izinParagraph = new Paragraph(izinMetni, bodyFont)
+                {
+                    Alignment = Element.ALIGN_LEFT
+                };
+                pdfDoc.Add(izinParagraph);
+
+                // PDF dokümanını kapat
+                pdfDoc.Close();
+
+                MessageBox.Show("PDF başarıyla oluşturuldu ve kaydedildi!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
